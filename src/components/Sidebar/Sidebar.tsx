1ecc0b448878c2d2e766d5471fb4fcf96eace1db
Stefano Straus <stefano@straus.it>
2026-02-20 19:53:19 +0100

    fix: make main branch asterisk icon bolder for visibility

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -34,7 +34,7 @@ export interface SidebarProps {
 const BranchIcon: Component<{ isMain: boolean; hasQuestion?: boolean }> = (props) => (
   <span class={`branch-icon ${props.hasQuestion ? "question" : props.isMain ? "main" : "feature"}`}>
     {props.hasQuestion ? "?" : props.isMain
-      ? <svg viewBox="0 0 16 16" width="13" height="13" fill="currentColor"><path d="M9 2.2V6.3l3.5-2a1 1 0 1 1 1 1.7L10 8l3.5 2a1 1 0 1 1-1 1.7L9 9.7v4.1a1 1 0 1 1-2 0V9.7l-3.5 2a1 1 0 1 1-1-1.7L6 8 2.5 6a1 1 0 0 1 1-1.7L7 6.3V2.2a1 1 0 0 1 2 0z"/></svg>
+      ? <svg viewBox="0 0 16 16" width="13" height="13" fill="currentColor"><path d="M9.2 1.5V5.8l3.7-2.1a1.2 1.2 0 1 1 1.2 2.1L10.4 8l3.7 2.1a1.2 1.2 0 1 1-1.2 2.1L9.2 10.2v4.3a1.2 1.2 0 1 1-2.4 0v-4.3l-3.7 2.1a1.2 1.2 0 1 1-1.2-2.1L5.6 8 1.9 5.9a1.2 1.2 0 0 1 1.2-2.1L6.8 5.8V1.5a1.2 1.2 0 0 1 2.4 0z"/></svg>
       : <svg viewBox="0 0 16 16" width="12" height="12" fill="currentColor"><path d="M11.75 2.5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5zm-2.25.75a2.25 2.25 0 1 1 3 2.122V6A2.5 2.5 0 0 1 10 8.5H6a1 1 0 0 0-1 1v1.128a2.251 2.251 0 1 1-1.5 0V5.372a2.25 2.25 0 1 1 1.5 0v1.836A2.493 2.493 0 0 1 6 7h4a1 1 0 0 0 1-1v-.628A2.25 2.25 0 0 1 9.5 3.25zM4.25 12a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5zM3.5 3.25a.75.75 0 1 1 1.5 0 .75.75 0 0 1-1.5 0z"/></svg>}
   </span>
 );
